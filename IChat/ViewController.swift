fe245f448b7988a1546610cf6161071fcfb3677d
Eugene Kiselev <zhenya-kiselev-97@mail.ru>
2021-05-01 18:31:16 +0300

    Initial Commit

---
+++ IChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IChat
+//
+//  Created by Eugene Kiselev on 01.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
